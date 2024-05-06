d9a7ef880efd769116f47fde03346b75e1ef141f
izzathabib <muhdizat.h@gmail.com>
2024-05-06 15:18:46 +0800

    Finish CodeIgniter Template System

---
+++ application/views/templates/default/menu.php
@@ -0,0 +1,20 @@
+<ul class="nav flex-column">
+  <li class="nav-item">
+    <a class="nav-link active" href="#">Dashboard</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Orders</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Products</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#"> Customers </a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Reports</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Integrations</a>
+  </li>
+</ul>
